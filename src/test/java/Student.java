cfc4bd4364965c74a7b110232f734a45e76fe214
xiaoqian  gao <xiaoqian@student.agh.edu.pl>
2021-10-31 12:32:03 +0100

    Initial commit

---
+++ src/test/java/Student.java
@@ -0,0 +1,90 @@
+import java.util.Objects;
+
+public class Student  {
+    private String Name;
+    private String LastName;
+    private StudentCondition Status;
+    private int YearOfBirth;
+    private double NumberOfCredits;
+
+    public Student(String name, String lastName, StudentCondition status, int yearOfBirth, double numberOfCredits) {
+        Name = name;
+        LastName = lastName;
+        Status = status;
+        YearOfBirth = yearOfBirth;
+        NumberOfCredits = numberOfCredits;
+    }
+
+    public Student() {
+    }
+
+    public void print() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "Name='" + Name + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", Status=" + Status +
+                ", YearOfBirth=" + YearOfBirth +
+                ", NumberOfCredits=" + NumberOfCredits +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return getYearOfBirth() == student.getYearOfBirth() && Double.compare(student.getNumberOfCredits(), getNumberOfCredits()) == 0 && Objects.equals(getName(), student.getName()) && Objects.equals(getLastName(), student.getLastName()) && getStatus() == student.getStatus();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getLastName(), getStatus(), getYearOfBirth(), getNumberOfCredits());
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public StudentCondition getStatus() {
+        return Status;
+    }
+
+    public void setStatus(StudentCondition status) {
+        Status = status;
+    }
+
+    public int getYearOfBirth() {
+        return YearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        YearOfBirth = yearOfBirth;
+    }
+
+    public double getNumberOfCredits() {
+        return NumberOfCredits;
+    }
+
+    public void setNumberOfCredits(double numberOfCredits) {
+        NumberOfCredits = numberOfCredits;
+    }
+
+
+}
---
+++ src/test/java/StudentCondition.java
@@ -0,0 +1,5 @@
+public enum StudentCondition {
+    Present,
+    Sick,
+    Absent,
+}
